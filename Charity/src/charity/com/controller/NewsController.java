2475ff82c474169565da965887072ca2cf64c9a0
licod1997 <licod1997@gmail.com>
2017-08-01 17:50:49 +0900

    prevent news page exception

--- Charity/src/charity/com/controller/NewsController.java
+++ Charity/src/charity/com/controller/NewsController.java
@@ -12,11 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.persistence.NoResultException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.text.ParseException;
 
 /**
  * Created by Notebook on 30-Jul-17.
